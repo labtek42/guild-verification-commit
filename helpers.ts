61c17ead677595d6b6c43dfcdb7fbfa13b2f149a
labtek42 <71946299+labtek42@users.noreply.github.com>
2026-03-06 21:54:47 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T18:54:37.232Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T18:54:46.661Z
+Commit: Patch API endpoint error handling
