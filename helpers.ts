be9f2019f01df0c6d7465af1e39deded74fae8d8
labtek42 <71946299+labtek42@users.noreply.github.com>
2026-03-06 22:03:25 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:03:00.756Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T19:03:24.874Z
+Commit: Resolve timezone issues in date handling
