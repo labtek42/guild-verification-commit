138c735c03d3b3de0734d0780950431d93b38251
labtek42 <71946299+labtek42@users.noreply.github.com>
2026-03-06 22:02:30 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:02:17.644Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T19:02:29.784Z
+Commit: Resolve null pointer exception in user service
