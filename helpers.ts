e56cf575971af2b3f57b44fbfec22ee664369a0e
labtek42 <71946299+labtek42@users.noreply.github.com>
2026-03-06 22:02:39 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T18:54:46.661Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T19:02:39.111Z
+Commit: Fix race condition in async operations
